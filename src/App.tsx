26e81a40aa6ba44a14a0fd0d29f7164d011f367d
Ralf Sigmund <ralf.sigmund@gmail.com>
2025-02-04 16:43:02 +0100

    also remove imports and declarations that aren't used anymore.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import PriceTable from "./components/PriceTable";
 
 const queryClient = new QueryClient();
 
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
